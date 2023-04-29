1891db7d749eff2ad29f3849bfcbdb6de70e8f33
Jordi Casanella <jordikiter@hotmail.es>
2023-04-30 00:30:42 +0100

    Starting to implement Sorter

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,11 @@
-console.log('hello!!!');
+class Sorter {
+    constructor(public collection: number[]) {}
 
-const logSomething = () => {
-    console.log('something');
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    sort(): void {}
 }
 
-logSomething();
+const sorter = new Sorter([1,2,3]);
+sorter.sort();
+
+console.log(sorter.collection);
