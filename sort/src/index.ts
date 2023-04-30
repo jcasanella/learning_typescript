9fca841080d5eaf22e796838d23bad16ca13ac09
Jordi Casanella <jordikiter@hotmail.es>
2023-04-30 03:24:26 +0100

    Implementing sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,31 @@
 class Sorter {
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    sort(): void {}
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j=0; j < length - i - 1; j++) {
+                // if collection is a number[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j+1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j+1];
+                        this.collection[j+1] = temp;
+                    }
+                }
+
+                // if collection is a string
+                if (typeof this.collection === 'string') {
+
+                }
+            }
+        }
+    }
 }
 
-const sorter = new Sorter([1,2,3]);
+const sorter = new Sorter([1,4,2,0]);
 sorter.sort();
 
 console.log(sorter.collection);
