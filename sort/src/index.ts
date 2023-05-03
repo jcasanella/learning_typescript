52a18203852a23207fe4564a752ceeb2c75057b1
Jordi Casanella <jordikiter@hotmail.es>
2023-05-04 00:38:36 +0100

    First refactoring sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,31 +1,6 @@
-class Sorter {
-    constructor(public collection: number[] | string) {}
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from "./Sorter";
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    sort(): void {
-        const { length } = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j=0; j < length - i - 1; j++) {
-                // if collection is a number[]
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j+1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j+1];
-                        this.collection[j+1] = temp;
-                    }
-                }
-
-                // if collection is a string
-                if (typeof this.collection === 'string') {
-
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([1,4,2,0]);
+const sorter = new Sorter(new NumbersCollection([10,3,-5,0]));
 sorter.sort();
-
 console.log(sorter.collection);
