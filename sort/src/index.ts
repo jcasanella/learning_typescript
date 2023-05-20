45e986cdb8f237dc698b3f28e76b047d67f57afd
Jordi Casanella <jordikiter@hotmail.es>
2023-05-21 00:17:02 +0100

    Replace interface by abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,21 +1,19 @@
 import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 import { LinkedList } from "./LinkedList";
 
-const sorter = new Sorter(new NumbersCollection([10,3,-5,0]));
-sorter.sort();
-console.log(sorter.collection);
+const numbers = new NumbersCollection([10,3,-5,0]);
+numbers.sort();
+console.log(numbers);
 
-const sorter2 = new Sorter(new CharactersCollection('zxdab'));
-sorter2.sort();
-console.log(sorter2.collection);
+const characters = new CharactersCollection('zxdab');
+characters.sort();
+console.log(characters);
 
 const linked = new LinkedList();
 linked.add(10);
 linked.add(3);
 linked.add(-5);
 linked.add(0);
-const sorter3 = new Sorter(linked);
-sorter3.sort();
+linked.sort();
 linked.print();
