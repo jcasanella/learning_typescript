982c000480e3db1f1216143b2765c3be0f9e3008
Jordi Casanella <jordikiter@hotmail.es>
2023-09-29 09:28:22 +0100

    Event association

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,11 +1,22 @@
 export class UserForm {
     constructor(private readonly parent: Element) {}
 
+    onButtonClick(): void {
+        console.log('Hi button!!!');
+    }
+
+    eventsMap(): { [key: string]: () => void } {
+        return {
+            'click:button': this.onButtonClick
+        };
+    }
+
     template(): string {
         return `
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Click Me</button>
             </div>
         `;
     }
