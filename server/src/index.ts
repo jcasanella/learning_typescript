f3275f133d5f4c50235ea98bc39d71c99e113e49
Jordi Casanella <jordikiter@hotmail.es>
2023-10-23 01:00:26 +0100

    Create router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
-app.get('/', (req: Request, resp: Response) => {
-    resp.send(`
-        <div>
-            <h1>Hi hi!!!</h1>
-        </div>
-    `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 app.listen(3000, () => {
     console.log('Listening on port');
 });
